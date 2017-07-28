f39b4b22cfbb52ea07c25ed24501335ef8cae230
Urvi9Sharma <urvi2vasudha@gmail.com>
2017-07-29 00:54:05 +0530

    flaskApi nginx is working now

---
+++ flaskApi/app/views.py
@@ -0,0 +1,13 @@
+# dummyapp/app/__init__.py
+from flask import Flask
+app = Flask(__name__)
+import views
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')

43da590e980871685c378128ae1a5a8096a3ece3
Abhinav Kumar <abhinav@outlook.com>
2017-07-28 21:03:21 +0530

    Working flask-ask

--- lambda/potts.py
+++ lambda/potts.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+
 @ask.intent('HelloIntent')
-def hello(firstname):
-    speech_text = "Hello %s" % firstname
+def hello():
+    speech_text = "Hello"
     return statement(speech_text).simple_card('Hello', speech_text)
+
+@ask.launch
+def launched():
+    return question('Welcome to Potts, your own office assistant. Just like Tony Stark.')
 
 if __name__ == '__main__':
     app.run()

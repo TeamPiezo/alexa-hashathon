596b793dfbc597f6991da2949e216ed6d643f411
Abhinav Kumar <abhinav@outlook.com>
2017-07-29 01:13:09 +0530

    Cleanup and commented

--- lambda/potts.py
+++ lambda/potts.py
@@ -14,37 +14,58 @@ def hello():
     return statement(speech_text).simple_card('Hello', speech_text)
 
 
+# Actions
+
 @ask.intent('FindSlot')
 def find_slot(date=None):
-    speech_text = 'These are the '
-    # Make request here
+    """
+    If date is provided, find the free slots. Else ask for date and track using stage=='find_slot'
+    :param date:
+    :return:
+    """
     if not date:
         session.attributes['stage'] = 'book_slot'
         return question('You didn\'t specify the date. What date you would like to book?')
     else:
-        # Make request to
+        # Make request to api to book
         session.attributes['stage'] = 'find_slot'
         session.attributes['date'] = date
         return question('These are free slots for ' + date + ' Which one do you want me to book?')
 
+
+@ask.intent('BookSlot')
+def book_slot(slot_time=None):
+    """
+    If time is provided, book for that time. Else ask the user to try again.
+    :param slot_time:
+    :return:
+    """
+    # Make request here
+    if not slot_time:
+        return question('You didn\'t specify the time. Try again.')
+    else:
+        # Make request to API
+        session.attributes['stage'] = 'book_slot'
+        return question('I have booked this room for ' + slot_time + '. Are you sure?')
+
+
+@ask.intent('StartMeeting')
+def start_meeting():
+    """
+
+    :return:
+    """
+    # Send request to API to begin session and get current user
+    meeting = {}
+    return statement('Meeting has been started for .')
+
+# Contextual Intents
 
 @ask.intent('DateIntent')
 def get_date(date):
     stage = session.attributes['stage']
     if stage == 'find_slot':
         return find_slot(date)
-
-
-@ask.intent('BookSlot')
-def book_slot(time=None):
-    speech_text = 'These are the '
-    # Make request here
-    if not time:
-        return question('You didn\'t specify the date. What date you would like to book?')
-    else:
-        # Make request to API
-        session.attributes['stage'] = 'book_slot'
-        return question('I have booked this room for ' + time + '. Are you sure?')
 
 
 @ask.intent('AMAZON.YesIntent')
